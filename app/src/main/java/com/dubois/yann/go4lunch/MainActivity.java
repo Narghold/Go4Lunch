c52289c3b4de1366e16402ec80ae164ee3210096
Yann <n4ghold87@gmail.com>
2022-01-05 12:32:49 +0100

    Navigation Drawer
    Adding Navigation Drawer
    Profile picture, display name & email are show
    Sign out is implement in
    Settings and "Your Lunch" not implemented yet

--- app/src/main/java/com/dubois/yann/go4lunch/MainActivity.java
+++ app/src/main/java/com/dubois/yann/go4lunch/MainActivity.java
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.dubois.yann.go4lunch.databinding.ActivityMainBinding;
@@ -16,6 +19,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -33,25 +37,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         //Get the las account connected to app
-        FirebaseAuth mMAuth = FirebaseAuth.getInstance();
-        currentUser = mMAuth.getCurrentUser();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
-        Glide.with(this).load(currentUser.getPhotoUrl()).into(mBinding.imageView);
-
-        mBinding.twName.setText(currentUser.getDisplayName());
-
-        mBinding.button2.setOnClickListener(new View.OnClickListener() {
+        //AppBar
+        setSupportActionBar(mBinding.topAppBar);
+        mBinding.topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                /*
-                 *  Return to LoginActivity after sign-out
-                 *  Don't use finish() cause of SplashScreen
-                 *  If loginActivity wasn't launch, stop the app
-                 */
-                Intent mIntent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(mIntent);
+                  mBinding.drawerLayout.open();
             }
         });
+
+        //NavigationView
+        mBinding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.nd_logout:
+                        FirebaseAuth.getInstance().signOut();
+                        /*
+                         *  Return to LoginActivity after sign-out
+                         *  Don't use finish() cause of SplashScreen
+                         *  If loginActivity wasn't launch, stop the app
+                         */
+                        Intent mIntent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(mIntent);
+                        break;
+                    default:
+                        item.isChecked();
+                        mBinding.drawerLayout.close();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        //Header Navigation Drawer
+        View mHeaderView = mBinding.navView.getHeaderView(0);
+        TextView hndName = mHeaderView.findViewById(R.id.hnd_name);
+        TextView hndMail = mHeaderView.findViewById(R.id.hnd_mail);
+        ImageView hndProfilePicture = mHeaderView.findViewById(R.id.hdn_profile_picture);
+        hndName.setText(currentUser.getDisplayName());
+        hndMail.setText(currentUser.getEmail());
+        Glide.with(this).load(currentUser.getPhotoUrl()).circleCrop().into(hndProfilePicture);
+        
     }
 }
